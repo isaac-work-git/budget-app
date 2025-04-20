f41127c12f297c2786a4a3642bd62d82bca92961
isaacaarondavies <isaac.work.davies@gmail.com>
2025-04-20 00:49:05 -0600

    Starting over

--- src/routes/lucia/+page.server.ts
+++ src/routes/lucia/+page.server.ts
@@ -1,22 +1,22 @@
 import * as auth from '$lib/server/auth';
-import { fail, redirect } from '@sveltejs/kit';
+import { fail, redirect, type RequestEvent } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async (event) => {
+export const load: PageServerLoad = async (event: { locals: { user: any; }; }) => {
 	if (!event.locals.user) {
-		return redirect(302, '/demo/lucia/login');
+		return redirect(302, '/lucia/login');
 	}
 	return { user: event.locals.user };
 };
 
 export const actions: Actions = {
-	logout: async (event) => {
+	logout: async (event: RequestEvent<Partial<Record<string, string>>, string | null>) => {
 		if (!event.locals.session) {
 			return fail(401);
 		}
 		await auth.invalidateSession(event.locals.session.id);
 		auth.deleteSessionTokenCookie(event);
 
-		return redirect(302, '/demo/lucia/login');
+		return redirect(302, '/lucia/login');
 	}
 };
